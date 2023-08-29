3a9423584d27354ef09e1541e547ea629c3ddf4e
Justin Ireland <justinireland90@yahoo.com>
2023-08-29 12:54:50 -0400

    Chapter 5 Book 1

---
+++ loops-intro/main.js
@@ -0,0 +1,12 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", 
+"Josh", "Nikki", "Matt", "Dale", "Sydney"];
+
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+}
+
+var myInterest = ["Swimming", "Video Games", "Food", "Family time", "Movies"];
+
+for(var i = 0; i <  myInterest.length; i++){
+    console.log(`One of my interest is: ${myInterest[i]}.`)
+}
